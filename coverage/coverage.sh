d7da11d103a47330350979dc6d6b67708f88d82b
baskapteijn <baskapteijn@gmail.com>
2020-07-16 16:50:48 +0200

    Fix the NLST and RETR error responses
    Add additional coverage tests.

--- coverage/coverage.sh
+++ coverage/coverage.sh
@@ -23,11 +23,36 @@ rc=$?; if [[ $rc != 0 ]]; then exit_on_error $rc; fi
 # Execute the program and try to cover as much as possible
 # Ignore return codes, this is not a component test but purely for coverage
 {
+    # Start the server
     ./vs-ftp 127.0.0.1 2021 /tmp &
+
+    # Perform an `NLIST` (and `FEAT`, `USER` and `QUIT` command)
     lftp -p2021 127.0.0.1 -e "nlist;bye"
+
+    # Perform an `NLIST .` command
+    lftp -p2021 127.0.0.1 -e "nlist .;bye"
+
+    # Perform an `NLIST ..` command (which should fail because it's above the root path)
+    lftp -p2021 127.0.0.1 -e "nlist ..;bye"
+
+    # Perform a `CWD ..` command (which should fail because it's above the root path)
+    lftp -p2021 127.0.0.1 -e "cd ..;bye"
+
+    # Perform a `CWD ..` command (which should fail because it's above the root path)
+    lftp -u anon,@ -p2021 127.0.0.1 -e "nlist;bye"
+
+    # Create and retrieve an existing file
+    dd if=/dev/urandom of=/tmp/file.bin bs=1024 count=1024
+    wget ftp://127.0.0.1:2021//tmp/file.bin
+
+    # Try to retrieve a non-existing file
+    wget ftp://127.0.0.1:2021//tmp/not_existing_file.bin
+
     #TODO: add more lftp commands and/or wget transfers
+
+    # Kill all and any vs-ftp process by sending SIGTERM (which is handled properly)
     killall vs-ftp
-} &> /dev/null
+} # &> /dev/null
 
 # Output the coverage result
 {
